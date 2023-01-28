ccd5e7332cd864f884974ff406fe41cc9db540b8
vladya <vladyslav.davydov100@gmail.com>
2023-01-28 22:12:27 +0300

    Задача с произведением

---
+++ seminars4/semi3/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход число и выдает произведение от 1 до числа
+Console.WriteLine("Введите число: ");
+int A = Convert.ToInt32(Console.ReadLine());
+
+int MultiPly(int num)
+{
+    int start = 1;
+    for (int i = 1; i <= num; i++)  
+    {
+        start = i * (start);
+    }
+    return start;
+}
+
+Console.WriteLine($"Произведение чисел от 1 до {A} равна {MultiPly(A)}");
---
+++ seminars4/semi4/Program.cs
@@ -0,0 +1,6 @@
+﻿// Напишите программу, которая принимает на вход число и выдает количество цифр в числе
+
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+for (int i = 0; )
