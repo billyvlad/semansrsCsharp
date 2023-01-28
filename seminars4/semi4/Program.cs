a68549a5d5c54714314d00c1a6e836586ce0293d
vladya <vladyslav.davydov100@gmail.com>
2023-01-28 22:29:14 +0300

    Задание с количеством цифр в числе

--- seminars4/semi4/Program.cs
+++ seminars4/semi4/Program.cs
@@ -1,6 +1,12 @@
 ﻿// Напишите программу, которая принимает на вход число и выдает количество цифр в числе
 
-Console.WriteLine("Введите число: ");
-int num = Convert.ToInt32(Console.ReadLine());
+Console.Write( "Введите  число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
 int count = 0;
-for (int i = 0; )
+while (number > 0)
+{
+    number /= 10;
+    count++;
+}
+Console.WriteLine($"Количество цифр в числе равна {count}");
