cdee41c1afb6c0b7b6d51c8c4005608e03e0382d
vladya <vladyslav.davydov100@gmail.com>
2023-01-28 21:24:45 +0300

    Задание 1 в семинаре

--- seminars4/semi1/Program.cs
+++ seminars4/semi1/Program.cs
@@ -1,3 +1,17 @@
-﻿
+﻿// Напишите программу, которая принимает на вход число и 
+// выдает сумму чисел от 1 до числа.
 
+Console.WriteLine("Введите число: ");
+int A = int.Parse(Console.ReadLine());
 
+int sum(int num)
+{
+    int Count = 0;
+    for (int i = 1; i <= num; i++)
+    {
+        Count += i;
+    }
+    return Count;
+}
+
+Console.WriteLine($"Сумма чисел от 1 до {A} равна {sum(A)}");
