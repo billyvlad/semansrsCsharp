f0f00cc5c63ed26325b70d4f6a7f74636f9305b9
vladya <vladyslav.davydov100@gmail.com>
2023-01-28 21:12:14 +0300

    DZ3

---
+++ seminars4/DZ3/Program.cs
@@ -0,0 +1,34 @@
+﻿int lenArray = ReadInt("Введите массив: ");
+
+int[] randomArray = new int[lenArray];
+for (int i = 0; i < randomArray.Length; i++)
+{
+    randomArray[i] = new Random().Next(1, 100);
+    Console.Write(randomArray[i] + " ");
+}
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+
+
+//int size = new Random().Next(0, 100);
+//int[] array = new int[size];
+
+//for (int i = 0; i < array.Length; i++)
+//{
+//    array[i] = new Random().Next(0, 100);
+//}
+//Console.WriteLine(size);
+//PrintArray(array);
+
+//void PrintArray(int[] array)
+//{
+//    for (int i = 0; i < array.Length; i++)
+//    {
+//        Console.Write(array[i] + " ");
+//    }
+//}
--- seminars4/semi1/Program.cs
+++ seminars4/semi1/Program.cs
@@ -1,10 +1,3 @@
 ﻿
 
 
-
-
-Console.Write("Введите число A: ");
-int numberA = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите число B: ");
-int numberB = Convert.ToInt32(Console.ReadLine());
-
