c2d1273e958b37c66ef4a9d059f86d0d7cab89ed
vladya <vladyslav.davydov100@gmail.com>
2023-01-28 20:55:06 +0300

    DZ1

---
+++ seminars4/semi1/Program.cs
@@ -0,0 +1,10 @@
+﻿
+
+
+
+
+Console.Write("Введите число A: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
