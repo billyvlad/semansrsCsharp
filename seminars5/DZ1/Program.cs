dcbcffe620f7db6da79477e47c751e3953f951dc
vladya <vladyslav.davydov100@gmail.com>
2023-02-04 20:13:58 +0300

    Семинар 5 DZ1

---
+++ seminars5/DZ1/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 34: 
+//Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, 
+//которая покажет количество чётных чисел в массиве.
+
+
+int[] numbers = new int[6];
+FillArrayRandomNumbers(numbers);
+PrintArray(numbers);
+int count = 0;
+for (int i = 0; i < numbers.Length; i++)
+    if (numbers[i] % 2 == 0)
+    count++;
+    Console.WriteLine($"Количество чётных чисел в массиве - {count}");
+
+void FillArrayRandomNumbers(int[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+        numbers[i] = new Random().Next(100,1000);
+}
+void PrintArray(int[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++) 
+        Console.Write(numbers[i] + " ");
+             Console.WriteLine();
+}
---
+++ seminars5/semi3/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 33: Задайте массив. Напишите программу, которая определяет, присутствует ли заданное число в массиве
+
+int[] array = new int[5];
+FillArray(array);
+Console.Write("Введите число поиска: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(array.Contains(number)) Console.Write("Да");
+else Console.Write("Нет");
+
+void FillArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(1, 10);
+        Console.Write(array[i] + " ");
+    }
+Console.Write("] -> ");
+}
