9e17ab97d2e70242c7e6aa89fe02ca9efda2ea65
vladya <vladyslav.davydov100@gmail.com>
2023-02-04 20:30:04 +0300

    Семинар 5 DZ3

---
+++ seminars5/DZ3/Program.cs
@@ -0,0 +1,25 @@
+﻿double[] array = new double[6];
+  for (int i = 0; i < array.Length; i++ )
+  {
+    array[i] = new Random().Next(1, 100);
+    Console.Write(array[i] + " ");
+  }
+
+double maxNumber = array[0];
+double minNumber = array[0];
+
+  for (int i = 1; i < array.Length; i++)
+  {
+    if (maxNumber < array[i])
+    {
+      maxNumber = array[i];
+    }
+        if (minNumber > array[i])
+    {
+      minNumber = array[i];
+    }
+  }
+
+  double diff = maxNumber - minNumber;
+
+  Console.WriteLine($"Разница между между максимальным ({maxNumber}) и минимальным({minNumber}) элементами: {diff}");
