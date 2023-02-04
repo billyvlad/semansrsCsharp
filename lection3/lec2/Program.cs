06c39fec992cb22300ff673834ca3fcd3f7ba55b
vladya <vladyslav.davydov100@gmail.com>
2023-02-04 17:03:14 +0300

    Лекция 3 папка 2

---
+++ lection3/lec2/Program.cs
@@ -0,0 +1,21 @@
+﻿//string Method4(int count, string text)
+//{
+//    string result = string.Empty;
+//    for( int i = 0; i < count; i++)
+//    {
+//        result = result + text;
+//    }
+//    return result;
+//}
+
+//string res = Method4(10, "sdsdsd");
+//Console.WriteLine(res);
+
+for (int i = 2; i <= 10; i++)
+{
+    for (int j = 2; j <= 10; j++)
+    {
+        Console.WriteLine($"{i} x {j} = {i * j}");  // Таблица умножения
+    }
+    Console.WriteLine();
+}
