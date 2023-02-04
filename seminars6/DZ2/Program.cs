50f4c2ca89b6d035a3957c07f0ee3f403a67f4b8
vladya <vladyslav.davydov100@gmail.com>
2023-02-04 23:17:47 +0300

    Семинар 6 DZ2

---
+++ seminars6/DZ2/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите k1");
+int k1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите k2");
+int k2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите b1");
+int b1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите b2");
+int b2 = Convert.ToInt32(Console.ReadLine());
+int X = (b1 - b2) / (k1 - k2);
+int Y = (k2 * b1 - k1 * b2) / (k1 - k2);
+Console.WriteLine($"Координаты точки пересечения:  X = {X} , Y = {Y}");
