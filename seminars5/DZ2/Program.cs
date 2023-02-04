a9f36704b51eb8cfe56e32a14e209cab2f29f46a
vladya <vladyslav.davydov100@gmail.com>
2023-02-04 20:22:38 +0300

    Семинар 5 DZ2

---
+++ seminars5/DZ2/Program.cs
@@ -0,0 +1,24 @@
+﻿int[] numbers = new int[5];
+FillArrayRandomNumbers(numbers);
+PrintArray(numbers);
+int sum = 0;
+
+    for (int i = 0; i < numbers.Length; i+=2)
+        sum = sum + numbers[i];
+        Console.WriteLine($"Cумма элементов, стоящих на нечётных позициях - {sum}");
+
+void FillArrayRandomNumbers(int[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+    
+        numbers[i] = new Random().Next(1,10);
+    
+}
+void PrintArray(int[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        Console.Write(numbers[i] + " ");
+    }
+             Console.WriteLine();
+}
