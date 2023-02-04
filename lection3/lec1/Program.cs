464784e4ae03fdc9ef14039439470c75bf788362
vladya <vladyslav.davydov100@gmail.com>
2023-02-04 16:36:38 +0300

    Лекция 3 папка 1

---
+++ lection3/lec1/Program.cs
@@ -0,0 +1,49 @@
+﻿//1st Method без аргумента
+
+//void Method1()
+//{
+//    Console.WriteLine("Autor");
+//}
+//Method1();
+
+
+//2nd Method с аргументом
+
+//void Method2(string msg, int count)
+//{
+//    int i = 0;
+//    while (int < count )
+//    {
+//      Console.WriteLine(msg);
+//      i++;  
+//    }
+//}
+//Method2(msg: "Text", count:4);       4 - количество выводов
+
+// Методы, которые возвращают но ничего не принимают
+
+//int Method3()
+//{
+//    return DateTime.Now.Year;
+//}
+
+//int year = Method3();
+//Console.WriteLine(year);
+
+// Методы, которые что то принимают и что то возвращают
+
+//string Method4(int count, string text)
+//{
+//    int i = 0;
+//    string result = string.Empty;
+
+//    while (i < count)
+//    {
+//        result = result + text;
+//        i++;
+//    }
+//    return result;
+//}
+
+//string res = Method4(10, "sdsdsd");
+//Console.WriteLine(res);
