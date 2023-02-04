15aea70a5ed233003acb7d32905aa63fbf6b1f0b
vladya <vladyslav.davydov100@gmail.com>
2023-02-04 19:11:37 +0300

    Семинар 5 задача 32

---
+++ seminars5/semi2/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 32: Напишите программу замена элементов массива: положительные элементы замените на соответсвующие 
+// отрицательные и наоборот
+// -4, -8, 8, 2     4, 8, -8, -2
+int[] array = new int[4];
+
+Console.Write("[");
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(-4, 11);
+    Console.Write(array[i] + " ");
+}
+Console.Write("] -> ");
+
+Console.Write("[");
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] *= -1;
+    Console.Write(array[i] + " ");
+}
+Console.Write("]");
