e42503e44ac6d654b7e6f1caa2a29a1af7da3680
vladya <vladyslav.davydov100@gmail.com>
2023-02-06 19:19:56 +0300

    Лекция 4 двумерные массивы

---
+++ lection4/lec1/Program.cs
@@ -0,0 +1,32 @@
+﻿// двумерные массивы 
+//string[,] table = new string[2,3]
+//int[,] matrix = new int[5,8]
+
+//String.Empty
+
+//string[,] table = new string[2, 5];
+//table[1, 2] = "word";
+
+//for (int rows = 0; rows < 2; rows++)
+//{
+//    for (int columns = 0; columns < 5; columns++)
+//    {
+//        Console.WriteLine($"-{table[rows, columns]}-");
+//    }
+//}
+
+
+int[,] matrix = new int[3, 4];
+void PrintArray(int[,] matr)
+{
+for (int rows = 0; rows < 3; rows++)    // or rows < matrix.GetLength(0) - что бы передать 3 из условия
+    {
+    for (int i = 0; i < 4; i++)         // or i < matrix.GetLength(1) - что бы передать 4 из условия
+        {   
+            Console.Write($"-{matrix[rows, i]}-");
+        }
+        Console.WriteLine();
+    }
+}
+
+PrintArray(matrix);
--- seminars6/semi4/Program.cs
+++ seminars6/semi4/Program.cs
@@ -1,1 +1,9 @@
-﻿
+﻿// Напишите программу, которая будет преобразовывать десятичное число в двоичное
+
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+while (num > 0)
+{
+    num % 2
+}
