7c99f3cce304e9a069246409333a3d970ebccd6d
vladya <vladyslav.davydov100@gmail.com>
2023-02-17 00:47:15 +0300

    Семинар 9, project1

---
+++ seminars9/project1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+void count (int number)
+{
+    if (number < 0) Console.Write($"{number} не натуральное число");
+    if (number == 0) return;
+    Console.WriteLine("{0,4}", number);
+    count (number - 1);
+}
+
+count(number);
