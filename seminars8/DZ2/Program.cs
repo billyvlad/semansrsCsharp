14ac3843222a923c6fd87a1151287412d7a01fb3
vladya <vladyslav.davydov100@gmail.com>
2023-02-17 01:23:13 +0300

    Семинар 8, DZ2

---
+++ seminars8/DZ2/Program.cs
@@ -0,0 +1,64 @@
+﻿int m = InputNumbers("Введите m: ");
+int n = InputNumbers("Введите n: ");
+int range = InputNumbers("Введите диапазон: от 1 до ");
+
+int[,] array = new int[m, n];
+
+int minsum = 0;
+int sum = SumLineElements(array, 0);
+for (int i = 1; i < array.GetLength(0); i++)
+{
+  int temp = SumLineElements(array, i);
+  if (sum > temp)
+  {
+    sum = temp;
+    minsum = i;
+  }
+}
+
+Console.WriteLine($"\n{minsum+1} - строкa с наименьшей суммой ({sum})");
+
+
+int SumLineElements(int[,] array, int i)
+{
+  int sumLine = array[i,0];
+  for (int j = 1; j < array.GetLength(1); j++)
+  {
+    sumLine += array[i,j];
+  }
+  return sumLine;
+}
+
+int InputNumbers(string input)
+{
+  Console.Write(input);
+  int output = Convert.ToInt32(Console.ReadLine());
+  return output;
+}
+
+void CreateArray(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      array[i, j] = new Random().Next(range);
+    }
+  }
+}
+
+void WriteArray (int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      Console.Write(array[i,j] + " ");
+    }
+    Console.WriteLine();
+  }
+}
+
+
+CreateArray(array);
+WriteArray(array);
