14a4e5d9b8a48e14350579e8e9e52486e8d26af3
vladya <vladyslav.davydov100@gmail.com>
2023-02-17 00:56:31 +0300

    Семинар 9, project2

--- seminars9/project1/Program.cs
+++ seminars9/project1/Program.cs
@@ -1,4 +1,7 @@
-﻿Console.WriteLine("Введите число: ");
+﻿//Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. 
+//Выполнить с помощью рекурсии.
+
+Console.WriteLine("Введите число: ");
 int number = int.Parse(Console.ReadLine());
 
 void count (int number)
---
+++ seminars9/project1/project2/Program.cs
@@ -0,0 +1,21 @@
+﻿//Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+
+
+Console.WriteLine("Введите число M: ");
+int M = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число N: ");
+int N = int.Parse(Console.ReadLine());
+
+void sumnat (int M, int N, int sum)
+{
+    if (M > N)
+    {
+        Console.WriteLine($"Сумма натуральных элементов от М до N равна {sum}");
+        return;
+    }
+    sum = sum + (M++);
+    sumnat(M, N, sum);
+}
+
+sumnat(M,N,0);
